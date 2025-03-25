64d1860e82eef69d4cb34fae9dfe061ff6240fcc
Allwell Agwu-Okoro <47219269+allwelldotdev@users.noreply.github.com>
2025-03-25 21:31:32 +0100

    refactor util/__init__.py: using relative imports

--- 04-concurrent_web_req/util/__init__.py
+++ 04-concurrent_web_req/util/__init__.py
@@ -1,3 +1,12 @@
-from util.aiohttp_funcs import fetch_status
-from util.async_timer import async_timed
-from util.delay_functions import delay
+# .aiohttp_funcs depends on another util module (.async_timer) which would cause a circular import
+# in this current state, it's in linear order (therefore, no circular imports error):
+# util/__init__.py -> aiohttp_funcs.py -> async_timer.py
+#
+# learn more: https://grok.com/share/bGVnYWN5_1e3074af-6826-4933-814b-daf11a047b0b
+# https://grok.com/share/bGVnYWN5_7726b530-8556-4da5-af91-22c0ef6d16b4
+
+from .aiohttp_funcs import fetch_status
+from .async_timer import async_timed
+from .delay_functions import delay
+
+__all__ = [async_timed, delay, fetch_status]
