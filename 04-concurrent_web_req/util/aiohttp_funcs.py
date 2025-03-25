ecf1e336a1962ef8a31548c1943d32ea9b576925
Allwell Agwu-Okoro <47219269+allwelldotdev@users.noreply.github.com>
2025-03-25 21:33:19 +0100

    add sleep parameter to delay func frm util

--- 04-concurrent_web_req/util/aiohttp_funcs.py
+++ 04-concurrent_web_req/util/aiohttp_funcs.py
@@ -1,7 +1,13 @@
+import asyncio
+
 from aiohttp import ClientSession
 
+# could cause circular import issues, therefore, will decorate in the main script instead
+# from .async_timer import async_timed
 
-async def fetch_status(session: ClientSession, url: str) -> int:
+
+# @async_timed()
+async def fetch_status(session: ClientSession, url: str, delay: int = 0) -> int:
     """async aiohttp client-session function. Takes session and url and returns GET request status code
 
     Parameters
@@ -16,6 +22,7 @@ async def fetch_status(session: ClientSession, url: str) -> int:
     int
         HTTP GET request status code
     """
+    await asyncio.sleep(delay)
     # access session using async with statement, returns result/response
     async with session.get(url) as response:
         return response.status
